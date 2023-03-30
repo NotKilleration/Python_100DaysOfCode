4623762cb6e5a2dbb6cc4c7edb181bdfe89a1c66
NotKilleration <77356224+NotKilleration@users.noreply.github.com>
2023-03-30 09:51:35 +0530

    final commit

--- Day 16 - Coffee Machine in OOP/main.py
+++ Day 16 - Coffee Machine in OOP/main.py
@@ -1,3 +1,25 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+is_on = True
+
+while is_on:
+    user_input = input(f"What would you like? {Menu.get_items()} ")
+    if user_input == "report":
+        CoffeeMaker.report()
+        MoneyMachine.report()
+    elif user_input == "off":
+        is_on = False
+    # elif Menu.find_drink(user_input) == user_input:
+    #     drink = Menu.find_drink()
+    #     if CoffeeMaker.is_resource_sufficient(drink["ingredients"]):
+    #         if  MoneyMachine.make_payment(drink["cost"]):
+    #             CoffeeMaker.make_coffee(user_input, drink["ingredients"])
+    # else:
+    #    print("Something went wrong. Please try again.")
+    else:
+        drink = Menu.find_item(user_input)
+        if CoffeeMaker.is_resource_sufficient(drink) and MoneyMachine.make_payment(drink.cost):
+                CoffeeMaker.make_coffee(drink)
+    
