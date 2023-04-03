0e2cdcca37d5d2c498418155bc4ed84c628488a3
NotKilleration <77356224+NotKilleration@users.noreply.github.com>
2023-04-03 21:32:45 +0530

    final commit

--- Day 17 - Quiz Project/data.py
+++ Day 17 - Quiz Project/data.py
@@ -1,14 +1,70 @@
 question_data = [
-{"text": "A slug's blood is green.", "answer": "True"},
-{"text": "The loudest animal is the African Elephant.", "answer": "False"},
-{"text": "Approximately one quarter of human bones are in the feet.", "answer": "True"},
-{"text": "The total surface area of a human lungs is the size of a football pitch.", "answer": "True"},
-{"text": "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", "answer": "True"},
-{"text": "In London, UK, if you happen to die in the House of Parliament, you are entitled to a state funeral.", "answer": "False"},
-{"text": "It is illegal to pee in the Ocean in Portugal.", "answer": "True"},
-{"text": "You can lead a cow down stairs but not up stairs.", "answer": "False"},
-{"text": "Google was originally called 'Backrub'.", "answer": "True"},
-{"text": "Buzz Aldrin's mother's maiden name was 'Moon'.", "answer": "True"},
-{"text": "No piece of square dry paper can be folded in half more than 7 times.", "answer": "False"},
-{"text": "A few ounces of chocolate can to kill a small dog.", "answer": "True"}
-]
+    {"category":"Science: Computers",
+     "type":"boolean",
+     "difficulty":"easy",
+     "question":"The logo for Snapchat is a Bell.",
+     "correct_answer":"False",
+     "incorrect_answers":["True"]
+     },
+     {"category":"Science: Computers",
+     "type":"boolean",
+     "difficulty":"easy",
+     "question":"&quot;HTML&quot; stands for Hypertext Markup Language.",
+     "correct_answer":"True",
+     "incorrect_answers":["False"]
+     },
+     {"category":"Science: Computers",
+      "type":"boolean",
+      "difficulty":"easy",
+      "question":"The Windows 7 operating system has six main editions.",
+      "correct_answer":"True",
+      "incorrect_answers":["False"]
+      },
+      {"category":"Science: Computers",
+       "type":"boolean",
+       "difficulty":"easy",
+       "question":"The Windows ME operating system was released in the year 2000.",
+       "correct_answer":"True",
+       "incorrect_answers":["False"]
+       },
+       {"category":"Science: Computers",
+        "type":"boolean",
+        "difficulty":"medium",
+        "question":"The first dual-core CPU was the Intel Pentium D.",
+        "correct_answer":"False",
+        "incorrect_answers":["True"]
+        },
+        {"category":"Science: Computers",
+        "type":"boolean",
+        "difficulty":"medium",
+        "question":"The last Windows operating system to be based on the Windows 9x kernel was Windows 98.",
+        "correct_answer":"False",
+        "incorrect_answers":["True"]
+        },
+        {"category":"Science: Computers",
+        "type":"boolean",
+        "difficulty":"medium",
+        "question":"It&#039;s not possible to format a write-protected DVD-R Hard Disk.",
+        "correct_answer":"True","incorrect_answers":["False"]
+        },
+        {"category":"Science: Computers",
+        "type":"boolean",
+        "difficulty":"easy",
+        "question":"Linux was first created as an alternative to Windows XP.",
+        "correct_answer":"False","incorrect_answers":["True"]
+        },
+        {"category":"Science: Computers",
+        "type":"boolean",
+        "difficulty":"medium",
+        "question":"A Boolean value of &quot;0&quot; represents which of these words?",
+        "correct_answer":"False",
+        "incorrect_answers":["True"]
+        },
+        {"category":"Science: Computers",
+        "type":"boolean",
+        "difficulty":"hard",
+        "question":"The T-Mobile Sidekick smartphone is a re-branded version of the Danger Hiptop.",
+        "correct_answer":"True",
+        "incorrect_answers":["False"]
+       }
+    ]
