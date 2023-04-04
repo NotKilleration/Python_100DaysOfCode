187727bcfa379cae0d69626953ad4d5477ff6249
NotKilleration <77356224+NotKilleration@users.noreply.github.com>
2023-04-04 18:45:16 +0530

    init commit

---
+++ Day 18 - The Hirst Painting Project/turtle_main.py
@@ -0,0 +1,14 @@
+from turtle import Turtle as t, Screen
+
+colors = []
+screen = Screen()
+t.shape("turtle")
+t.color("Green")
+
+# for i in range(10):   
+#     tim.forward(20)
+#     tim.penup()
+#     tim.forward(20)
+#     tim.pendown()
+
+screen.exitonclick()
