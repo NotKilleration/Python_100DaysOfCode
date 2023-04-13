8120a267e56db592d5886db44c69a8911b4b4af9
NotKilleration <77356224+NotKilleration@users.noreply.github.com>
2023-04-13 23:06:04 +0530

    paddle collison detection and bug fixes

--- Day 22 - Pong/ball.py
+++ Day 22 - Pong/ball.py
@@ -13,7 +13,11 @@ class Ball(Turtle):
     def move(self):
         new_x = self.xcor() + self.x_move
         new_y = self.xyor() + self.y_move
+        self.goto(new_x, new_y)
 
-    def bounce(self):
+    def bounce_y(self):
         self.y_move *= -1
+        
+    def bounce_x(self):
+        self.x_move *= -1
         
