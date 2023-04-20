24aed3be4ea7f83a821fb1cd4256a4493aeff8b4
Alok Kumar Mishra <77356224+NotKilleration@users.noreply.github.com>
2023-04-20 22:03:14 +0530

    update scoreboard

--- Day 20 & 21 - Snake Game/scoreboard.py
+++ Day 20 & 21 - Snake Game/scoreboard.py
@@ -9,6 +9,7 @@ class Scoreboard(Turtle):
         super().__init__()
 
         self.score = 0
+        self.high_score = 0
         self.penup
         self.goto(0, 270)
         self.color("white")
@@ -16,14 +17,17 @@ class Scoreboard(Turtle):
         self.hideturtle
 
     def update_scoreboard(self):
+        self.clear()
         self.write(f"Score: {self.score}", align = ALIGNMENT, font= FONT)
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("GAME OVER.", align= ALIGNMENT, font = FONT)
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
+        self.update_scoreboard()
 
 
     def increase_score(self):
         self.score += 1
-        self.clear()
         self.update_scoreboard()
